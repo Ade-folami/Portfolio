b66ef131d2ebe695e72a6a2bd03c2159544a7eb9
Adefolami <akintokunadefolami@gmail.com>
2024-06-28 22:05:25 +0100

    Added some Javascript and CSS functionality

--- script.js
+++ script.js
@@ -71,3 +71,23 @@ window.addEventListener("scroll", function (e) {
   if (window.scrollY > initialCoords.top) header.classList.add("sticky");
   else header.classList.remove("sticky");
 });
+
+//Reveal sections on scroll
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
